a81ea5159f9da72a10e04e7922d8b2c8ef5ccf0a
ASUS-ROG-STRIX\TAYYA <tayyab19jr@gmail.com>
2022-05-17 15:54:46 +0500

    Added Models

--- project/backend/api/models/products.py
+++ project/backend/api/models/products.py
@@ -1,3 +1,25 @@
+from ast import Try
+from pyexpat import model
+import uuid
+from xml.dom.minidom import CharacterData
 from django.db import models
 
-# Create your models here.
+class Product(models.Model): 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sku = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField(max_length=10)
+    product_url = models.URLField()
+    image_url = models.URLField()
+    category = models.CharField(100)
+    approved = models.BooleanField()
+    suspended = models.BooleanField()
+    time_stamp = models.DateTimeField()
+    quantity = models.FloatField(max_length=7)
+    
+class ProductCharacteristics(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    color = models.CharField(max_length=50)
+    size = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product)
